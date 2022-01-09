e2460a2e706002601b04c68539353009f110735e
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-01-08 22:29:26 -0500

    reworded questions and answers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const utils = require('./utils/generateMarkdown');
-const generateMarkdown = require('./utils/generateMarkdown');
+const utils = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 const fileName = 'README.md';
 
 const questions = () => {
@@ -9,45 +9,50 @@ const questions = () => {
         .prompt([
             {
                 type: 'input',
-                 message: 'What is the project title?',
-                 name: 'title',
-             },
-            {
-                  type: 'input',
-                 message: 'Provide a description',
-                 name: 'description',
+                message: 'What is the project title?',
+                name: 'title',
              },
             {
                 type: 'input',
-                message: 'Provide installation instructions',
+                message: 'Provide a short description of your project',
+                name: 'description',
+             },
+            {
+                type: 'checkbox',
+                message: 'Which command should be run to install dependencies?',
                 name: 'installation instructions',
+                choices: ['npm i'],
+                default: 'nmp i',
             },
             {
                 type: 'input',
-                message: 'Provide usage information',
+                message: 'What information does the user need to know about using the repo?',
                 name: 'usage information',
             },
             {
                 type: 'input',
-                message: 'Provide contribution guidelines',
+                message: 'What does the user need to know about contributing to the repo?',
                 name: 'contribution guidelines',
             },
             {
                 type: 'input',
-                message: 'Provide test instructions',
+                message: 'Which command should be run to run tests?',
                 name: 'test instructions',
+                choices: ['npm test'],
+                default: 'nmp test',
             },
             {
                 type: 'checkbox',
                 message: 'Which license did you use?',
                 name: 'license',
                 choices: [
-                'The MIT License', 
-                'The GPL License', 
-                'The Apache License', 
-                'The GNU License', 
+                'MIT', 
+                'GPL 3.0', 
+                'Apache 2.0',
+                'BSD 3', 
+                'GNU', 
                 'N/A'],
-                default: 'The MIT License',
+                default: 'MIT',
             },
             {
                 type: 'input',
